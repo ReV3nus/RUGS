d745bb7750d01b2bca96e0bd8ad7cc099cf0b10b
Weike Qian <125937302+ReV3nus@users.noreply.github.com>
2026-03-24 20:43:47 -0400

    Reorder InputSplatData into original GSIR format

--- package/Editor/Utils/GaussianFileReader.cs
+++ package/Editor/Utils/GaussianFileReader.cs
@@ -15,10 +15,10 @@ namespace GaussianSplatting.Editor.Utils
     public struct InputSplatData
     {
         public Vector3 pos;
-        public Vector3 nor;
         public Vector3 dc0;
         public Vector3 sh1, sh2, sh3, sh4, sh5, sh6, sh7, sh8, sh9, shA, shB, shC, shD, shE, shF;
         public float opacity;
+        public Vector3 nor;
         public Vector3 albedo;
         public float roughness;
         public float metallic;
@@ -86,7 +86,7 @@ namespace GaussianSplatting.Editor.Utils
         static unsafe void ReorderSHs(int splatCount, float* data)
         {
             int splatStride = UnsafeUtility.SizeOf<InputSplatData>() / 4;
-            int shStartOffset = 9, shCount = 15;
+            int shStartOffset = 6, shCount = 15;
             float* tmp = stackalloc float[shCount * 3];
             int idx = shStartOffset;
             for (int i = 0; i < splatCount; ++i)
